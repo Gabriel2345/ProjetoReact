eeba0a3b79f1c29d4f4a415e548a1e548c95a871
Gabriel2345 <gabriel_r_goncalves@hotmail.com>
2024-06-25 13:42:13 -0300

    Criando conexão com o banco

--- API/server.js
+++ API/server.js
@@ -1,23 +1,27 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
 const users = []
 
 app.post('/users', (req, res) => {
 
-    console.log(req)
+    users.push(req.body)
 
-    res.send('Ok post')
+    res.status(201).json(req.body)
 
 })
 
 app.get('/users', (req, res) => {
-    res.send('Ok')
+    res.status(200).json(users)
 })
 
 app.listen(3000)
 /* 
  1) tipo de rota/método HTTP
  2) endereço
+
+ username - gabriel
+ senha - 2z02yCIU38YNn7VL
 */
