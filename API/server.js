6df18c7746f81a46a716b016203efe38828a5a91
Gabriel2345 <gabriel_r_goncalves@hotmail.com>
2024-07-04 13:25:31 -0300

    Rotas atualizar e deletar

--- API/server.js
+++ API/server.js
@@ -6,8 +6,6 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-const users = []
-
 app.post('/users', async (req, res) => {
 
     await prisma.user.create({
@@ -23,11 +21,42 @@ app.post('/users', async (req, res) => {
 
 })
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+    
+    const users = await prisma.user.findMany()
+
     res.status(200).json(users)
 })
 
-app.listen(3001)
+app.put('/users/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },    
+        data: {
+                email: req.body.email,
+                name: req.body.name,
+                age: req.body.age
+            }
+
+    })
+
+    res.status(201).json(req.body)
+
+})
+
+app.delete('/users/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+        res.status(200).json({message: 'Usuário deletado com sucesso!'})
+})
+
+app.listen(3000)
 /* 
  1) tipo de rota/método HTTP
  2) endereço
