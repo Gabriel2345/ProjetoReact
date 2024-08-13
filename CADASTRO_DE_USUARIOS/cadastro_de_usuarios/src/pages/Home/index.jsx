80bb462389f6f3defa57d13fd77945d9f965f2af
Gabriel2345 <gabriel_r_goncalves@hotmail.com>
2024-08-13 13:38:32 -0300

    fazendo map para listar usuários

--- CADASTRO_DE_USUARIOS/cadastro_de_usuarios/src/pages/Home/index.jsx
+++ CADASTRO_DE_USUARIOS/cadastro_de_usuarios/src/pages/Home/index.jsx
@@ -2,32 +2,43 @@ import './style.css'
 import Trash from '../../assets/trash.png'
 
 function Home() {
-  
+
+  const users = [{
+    id: '',
+    name: '',
+    age:'',
+    email
+  }, {
+
+  }]
 
   return (
-  
-      <div className='container'>
-        <form action="">
-          <h1>Cadastro de usuários</h1>
-          <input name='name'type='text'/>
-          <input name='age' type='number' />
-          <input name='email' type='email'/>
-          <button type='button'>Cadastrar</button>
-        </form>
 
-        <div>
+    <div className='container'>
+      <form action="">
+        <h1>Cadastro de usuários</h1>
+        <input name='name' type='text' />
+        <input name='age' type='number' />
+        <input name='email' type='email' />
+        <button type='button'>Cadastrar</button>
+      </form>
+
+      {users.map((user) => (
+        <div key={user.id}>
           <div>
-            <p>Nome: </p>
-            <p>Idade: </p>
-            <p>E-mail: </p>
+            <p>Nome: {user.name}</p>
+            <p>Idade: {user.age}</p>
+            <p>Email: {user.email}</p>
             <button>
-              <img src={Trash}/>
+              <img src={Trash} />
             </button>
           </div>
         </div>
-      </div>
-      
-  
+      ))}
+
+    </div>
+
+
   )
 }
 
